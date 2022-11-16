3dfe75035941dbc6e5f0660bc773af8ad692d606
G.Mohan Teja <mohanteja.g2019@vitstudent.ac.in>
2022-11-17 00:59:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# StudentGradeSheetEvaluvator
+ Converts VIT grades to US 4 point scale when given VIT Grade sheet
---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from tabula import read_pdf
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
+#reads table from pdf file
+# df = read_pdf("abc.pdf",pages="all") #address of pdf file
+# print(tabulate(df))
+header = lambda df:df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True)
+
+st.title("VIT grade sheet to US 4 point scale converter")
+st.write("Only covers course options 'NIL'")
+gradeScale = pd.read_csv("grade.csv")
+grade_to_gpa = dict(gradeScale[['Grade','Scale']].values)
+grade_to_us = dict(gradeScale[['Grade','US Grade Points']].values)
+
+uploaded_file = st.file_uploader("Upload your grade sheet")
+if uploaded_file is not None:
+    dfs = read_pdf(uploaded_file,pages="all")
+    dfs = dfs[:-3]
+    nd = []
+    for df in dfs:
+        nd.append(header(df))
+    data = pd.concat(nd,ignore_index=True).drop(columns=['Sl.No'])
+    data["10 Scale"] = data["Grade"].replace(grade_to_gpa)
+    data["4 Scale"] = data["Grade"].replace(grade_to_us)
+    data["Final Credits"] = np.where(data['Course\rOption'] == 'NIL',data['Credits'],0)
+    data['10 scale weights'] = data['10 Scale'].astype(float)*data['Final Credits'].astype(float)
+    data['4 scale weights'] = data['4 Scale'].astype(float)*data['Final Credits'].astype(float)
+    st.write(data)
+    
+    col1, col2,col3 = st.columns(3)
+    col3.metric("Total credits Earned",round(np.sum(data["Final Credits"].astype(float).values)))
+    col1.metric("VIT CGPA",round(data['10 scale weights'].astype(float).sum()/data["Final Credits"].astype(float).sum(),2))
+    col2.metric("USA CGPA",round(data['4 scale weights'].astype(float).sum()/data["Final Credits"].astype(float).sum(),2))
+st.write("Grade Scale : ")
+st.write(header(gradeScale.transpose()))
+
